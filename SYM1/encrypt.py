31bfbf5886904a1ec39d2035fd3587c8952fb5cb
Marc PEREZ <marc.perez@epitech.eu>
2020-11-17 21:16:33 +0100

    Annotated SYM1 with the answers

--- SYM1/encrypt.py
+++ SYM1/encrypt.py
@@ -16,7 +16,7 @@ PLAINTEXT = "I was lost, but now I'm found...".encode()
 assert len(PLAINTEXT) == 32
 
 KEY = hashlib.sha256("omgwtfbbq".encode()).digest()
-iv = ''.encode() + bytes(16)
+iv = 'h4ppyh4ppya5canB'.encode()
 assert len(iv) == 16
 
 cipher = AES.new(KEY, AES.MODE_CBC, iv)
@@ -29,10 +29,10 @@ if __name__ == '__main__':
 
 ENCODED = bytes([
     # Unknown
-    0x00,
+    0x00,  # 0x02
     0xf3, 0x74, 0xa8, 0x2d, 0xb5, 0x0b, 0x23,
     # Unknown
-    0x00, 0x00, 0x0b,
+    0x00, 0x00, 0x0b,  # 0xa7, 0xd0, 0x9b
     0x88, 0xf1, 0xd9, 0x76, 0xdd, 0xc1, 0xcf, 0x6d, 0xb4, 0x52, 0x4a, 0xac, 0x04, 0xe2, 0x22, 0x85, 0x39, 0x69, 0x36, 0x7e, 0x0d
 ])
 assert len(ENCODED) % 16 == 0
@@ -91,14 +91,8 @@ def bruteforce_encoded_bytes(encoded_0: bytes):
             return ({
                 'new_cipher_text': new_cipher_text,
                 'bruteforced_encoded': bruteforced_encoded,
-                'found_iv': found_iv
+                'found_iv': found_iv  # h4ppyh4ppya5canB
             })
-            # print('new_cipher_text:')
-            # print(binascii.hexlify(new_cipher_text))
-            # print('encoded (bruteforced):')
-            # print(binascii.hexlify(bruteforced_encoded))
-            # print("IV:")
-            # print(foundIV[:16])
 
 
 if __name__ == '__main__':
